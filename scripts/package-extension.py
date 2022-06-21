d9b65d01657719310e7c97d3d602587eb2f470dd
David Brown <tngamerboy@gmail.com>
2022-06-20 20:17:52 -0500

    Add main methods to Python scripts (#2)

--- scripts/package-extension.py
+++ scripts/package-extension.py
@@ -1,22 +1,26 @@
 import zipfile
 import pathlib
 
-directory = pathlib.Path.cwd()
-filenames = ["LICENSE", "manifest.json"]
+def main():
+    directory = pathlib.Path.cwd()
+    filenames = ["LICENSE", "manifest.json"]
 
-with zipfile.ZipFile("extensions/chromium/web-input-unlocked.zip", mode="w") as archive:
-    for file_path in directory.glob("src/*"):
-        archive.write(file_path, arcname=file_path.relative_to(directory))
-    for file_path in directory.glob("popups/*"):
-        archive.write(file_path, arcname=file_path.relative_to(directory))
-    for file_path in directory.glob("node_modules/*"):
-        archive.write(file_path, arcname=file_path.relative_to(directory))
-    for file_path in directory.glob("assets/*"):
-        archive.write(file_path, arcname=file_path.relative_to(directory))
-    for file_path in directory.glob("icons/*"):
-        archive.write(file_path, arcname=file_path.relative_to(directory))
-    for filename in filenames:
-        archive.write(filename)
+    with zipfile.ZipFile("extensions/chromium/web-input-unlocked.zip", mode="w") as archive:
+        for file_path in directory.glob("src/*"):
+            archive.write(file_path, arcname=file_path.relative_to(directory))
+        for file_path in directory.glob("popups/*"):
+            archive.write(file_path, arcname=file_path.relative_to(directory))
+        for file_path in directory.glob("node_modules/*"):
+            archive.write(file_path, arcname=file_path.relative_to(directory))
+        for file_path in directory.glob("assets/*"):
+            archive.write(file_path, arcname=file_path.relative_to(directory))
+        for file_path in directory.glob("icons/*"):
+            archive.write(file_path, arcname=file_path.relative_to(directory))
+        for filename in filenames:
+            archive.write(filename)
 
-with zipfile.ZipFile("extensions/chromium/web-input-unlocked.zip", mode="r") as archive:
-    archive.printdir()
+    with zipfile.ZipFile("extensions/chromium/web-input-unlocked.zip", mode="r") as archive:
+        archive.printdir()
+
+if __name__ == '__main__':
+    main()
