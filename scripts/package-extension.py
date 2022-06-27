82017c48220845313126531ce9a262d976a10685
Adobe-Android <tngamerboy@gmail.com>
2022-06-26 21:49:18 -0500

    Fix bug in extension packaging script

--- scripts/package-extension.py
+++ scripts/package-extension.py
@@ -4,13 +4,12 @@ import pathlib
 def main():
     directory = pathlib.Path.cwd()
     filenames = ["LICENSE", "manifest.json"]
+    node_modules_directory = pathlib.Path("node_modules")
 
     with zipfile.ZipFile("extensions/chromium/web-input-unlocked.zip", mode="w") as archive:
         for file_path in directory.glob("src/*"):
             archive.write(file_path, arcname=file_path.relative_to(directory))
         for file_path in directory.glob("popups/*"):
-            archive.write(file_path, arcname=file_path.relative_to(directory))
-        for file_path in directory.glob("node_modules/*"):
             archive.write(file_path, arcname=file_path.relative_to(directory))
         for file_path in directory.glob("assets/*"):
             archive.write(file_path, arcname=file_path.relative_to(directory))
@@ -18,6 +17,8 @@ def main():
             archive.write(file_path, arcname=file_path.relative_to(directory))
         for filename in filenames:
             archive.write(filename)
+        for file_path in node_modules_directory.rglob("*"):
+            archive.write(file_path, arcname=file_path)
 
     with zipfile.ZipFile("extensions/chromium/web-input-unlocked.zip", mode="r") as archive:
         archive.printdir()
